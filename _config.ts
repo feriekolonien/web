2838263a702e5b5e2d717c6a7e1891ef1f82914e
Rikard Eide <rikardeide@gmail.com>
2025-10-12 13:32:28 +0200

    configure DNS/CNAME

--- _config.ts
+++ _config.ts
@@ -1,15 +1,17 @@
-import lume from "lume/mod.ts";
-import metas from "lume/plugins/metas.ts";
-import nav from "lume/plugins/nav.ts";
-import robots from "lume/plugins/robots.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
-import sitemap from "lume/plugins/sitemap.ts";
-import gzip from "lume/plugins/gzip.ts";
+import lume from 'lume/mod.ts';
+import metas from 'lume/plugins/metas.ts';
+import nav from 'lume/plugins/nav.ts';
+import robots from 'lume/plugins/robots.ts';
+import tailwindcss from 'lume/plugins/tailwindcss.ts';
+import sitemap from 'lume/plugins/sitemap.ts';
+import gzip from 'lume/plugins/gzip.ts';
 
-const site = lume();
+const site = lume({
+  location: new URL('https://github.com/feriekolonien/web'),
+});
 
-site.add("/assets");
-site.add("./favicon.ico");
+site.add('/assets');
+site.add('./favicon.ico');
 
 site.use(metas());
 site.use(nav());
